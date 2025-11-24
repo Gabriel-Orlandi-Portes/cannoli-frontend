5b85722be069bc94bef7f23eb3dd11c57af551fe
Gabriel <Gabrielorlandi220@gmail.com>
2025-11-24 19:00:25 -0300

    Fix GitHub Pages routing and add 404.html

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,37 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import AdminDashboard from "./pages/AdminDashboard";
+import ClienteDashboard from "./pages/ClienteDashboard";
+import CampaignSimulator from "./pages/CampaignSimulator";
+import Faturamento from "./pages/Faturamento";
+import Lojas from "./pages/Lojas";
+import TempoStatus from "./pages/TempoStatus";
+import ClientePedidos from "./pages/ClientePedidos";
+import ClientePerfil from "./pages/ClientePerfil";
+import ClientePromocoes from "./pages/ClientePromocoes";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+export default function App() { 
+  return (  
+    <Router basename="/cannoli-frontend">  
+      <Routes>  
+        {/* 🏠 Página inicial */} 
+        <Route path="/" element={<Login />} />
+
+        {/* 🧑‍💼 Dashboard do Admin */}
+        <Route path="/admin" element={<AdminDashboard />} />
+
+        {/* 🌿 Dashboard do Cliente */}
+        <Route path="/cliente" element={<ClienteDashboard />} />
+        <Route path="/cliente/pedidos" element={<ClientePedidos />} />
+        <Route path="/cliente/perfil" element={<ClientePerfil />} />
+        <Route path="/cliente/promocoes" element={<ClientePromocoes />} />
+
+        <Route path="/admin/simulador" element={<CampaignSimulator />} />
+        <Route path="/admin/faturamento" element={<Faturamento />} />
+        <Route path="/admin/lojas" element={<Lojas />} />
+        <Route path="/admin/tempo" element={<TempoStatus />} />
+      </Routes>
+    </Router>
+  );
+}
